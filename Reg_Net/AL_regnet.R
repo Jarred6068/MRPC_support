c2d9cd8b7c5b4a71259d9e6a9f2896947556cfed
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2021-03-11 21:39:57 -0800

    updates to regnet and the write up

--- Reg_Net/AL_regnet.R
+++ Reg_Net/AL_regnet.R
@@ -1,3 +1,13 @@
+#load in previous files
+# source("/mnt/ceph/jarredk/HiC_Analyses/HiC_search.R")
+# 
+# tissues.vec=tissue.names[,2]
+# for(t in 1:length(tissues.vec)){
+#   M1.Data=ADDIS.M1.check(tissue.names=tissues.vec[t])
+#   
+#   save(M1.Data, file = paste("/mnt/ceph/jarredk/Reg_Net/AL_M1Data/",tissues.vec[t],".Rdata",sep = ""))
+# 
+# }
 
 
 #===========================================================================================================
@@ -5,7 +15,7 @@
 #A function to extract all associated tissues and M classes for a given trio
 
 
-uncover_net=function(trio_number=NULL, FDR="ADDIS", verbose=TRUE){
+uncover_net=function(tissue.selection=1, FDR="ADDIS", mediator="trans", verbose=TRUE){
   #SYNTAX:
   #trio_number -- the index of the trio you wish to search
   #FDR -- designates which list data to search in (LOND or ADDIS)
@@ -14,71 +24,189 @@ uncover_net=function(trio_number=NULL, FDR="ADDIS", verbose=TRUE){
   #load in previous files
   source("/mnt/ceph/jarredk/HiC_Analyses/HiC_search.R")
   
-  tissues.vec=tissue.names[,2]
+  tissues.vec=tissue.names[tissue.selection,2]
+  all.tissues=tissue.names[,2]
+  loaded.datasets=list()
   
-  data1=as.data.frame(matrix(0, nrow=length(tissues.vec), ncol = 2))
-  names(data1)=c("Tissue", "Model")
-  logivec=NULL
-  not_in_tiss=rep(FALSE, 5)
+  #loading bar
+  lbar <- txtProgressBar(min = 0, max = length(all.tissues), style = 3)
+  #load in all datasets
+  for(i in 1:length(all.tissues)){
+    
+    loaded.datasets[[i]]=loadRData(fileName = paste("/mnt/lfs2/mdbadsha/peer_example/SNP_cis_trans_files/GTEx_version_8/", 
+                                                    all.tissues[i],"_AllPC/data.snp.cis.trans.final.",all.tissues[i],".V8.unique.snps.RData", sep = ""))
+    
+    Sys.sleep(0.05)
+    setTxtProgressBar(lbar, i)
+    
+  }
+  #close loading bar
+  close(lbar)
+  print("--Data:--loading--complete--")
   
+  #preallocate space
+  matchtab1=as.data.frame(matrix(0, nrow = length(tissues.vec), ncol = 2))
+  colnames(matchtab1)=c("tissue", "found")
+  all.matches=list()
   
+  #begin for all tissues in tissues selection
   for( t in 1:length(tissues.vec)){
     
     trioData=loadRData(fileName = paste("/mnt/lfs2/mdbadsha/peer_example/SNP_cis_trans_files/GTEx_version_8/", 
                                          tissues.vec[t],"_AllPC/data.snp.cis.trans.final.",tissues.vec[t],".V8.unique.snps.RData", sep = ""))
     
-    addisData=ADDIS.M1.check(tissue.names=tissues.vec[t])
-  
-    #get the M-types (M0, M1, ...)
-    Mtypes=names(L1.lond)
+    M1.Data=loadRData(fileName = paste("/mnt/ceph/jarredk/Reg_Net/AL_M1Data/", tissues.vec[t],".Rdata", sep = ""))
     
-    for(i in 1:length(L1.lond)){
+    addisM1 = M1.Data$Catalog[[1]][[1]][[2]]
+    londM1 =  M1.Data$Catalog[[1]][[1]][[1]]
+#=========================================================
+#========================ADDIS============================
+    if(FDR=="ADDIS"){
       
-      #match the trio number to trios in each list under Mtypes
-      if(FDR=="ADDIS"){
+#-------------------Addis-Trans---------------------------
+      if(mediator=="trans"){
+        
+        matchtab2=list()
+        
+        for(j in 1:length(addisM1$type2)){
+          
+          index=(addisM1$type2[j]-1)*3+(1:3)
+          members=colnames(trioData[,index])[3]
+          print(members)
+          
+          for(k in 1:length(all.tissues)){
+            
+            trioData2=colnames(loaded.datasets[[k]])
+            
+            matched=match(members, trioData2)
+            #print(matched)
+            
+            matchtab1[k,1]=ifelse(is.na(matched)==TRUE, NA, all.tissues[k])
+            matchtab1[k,2]=ifelse(is.na(matched)==TRUE, 0, 1)
 
-        matched=match(trio_number, L2.addis[[i]])
+          }
+          
+          matchtab2[[j]]=matchtab1
+          #print(matchtab2)
+        }
         
-        logivec[i]=ifelse(is.na(matched)==TRUE, FALSE, TRUE)
+        all.matches[[t]]=matchtab2
+        #print(all.matches)
         
-        
+#------------------addis-cis------------------------        
       }else{
         
-        matched=match(trio_number, L1.lond[[i]])
+        matchtab2=list()
         
-        logivec[i]=ifelse(is.na(matched)==TRUE, FALSE, TRUE)
+        for(j in 1:length(addisM1$type1)){
+          
+          index=(addisM1$type1[j]-1)*3+(1:3)
+          members=colnames(trioData[,index])[3]
+          print(members)
+          
+          for(k in 1:length(all.tissues)){
+            
+            trioData2=colnames(loaded.datasets[[k]])
+            
+            matched=match(members, trioData2)
+            #print(matched)
+            
+            matchtab1[k,1]=ifelse(is.na(matched)==TRUE, NA, all.tissues[k])
+            matchtab1[k,2]=ifelse(is.na(matched)==TRUE, 0, 1)
+            
+          }
+          
+          matchtab2[[j]]=matchtab1
+          #print(matchtab2)
+        }
         
+        all.matches[[t]]=matchtab2
+        #print(all.matches)
         
       }
       
+#=====================================================
+#======================LOND===========================
+
+    }else{
+      
+#------------------lond---trans-----------------------   
+      if(mediator=="trans"){
+        
+        matchtab2=list()
+        
+        for(j in 1:length(londM1$type2)){
+          
+          index=(londM1$type2[j]-1)*3+(1:3)
+          members=colnames(trioData[,index])[3]
+          print(members)
+          
+          for(k in 1:length(all.tissues)){
+            
+            trioData2=colnames(loaded.datasets[[k]])
+            
+            matched=match(members, trioData2)
+            #print(matched)
+            
+            matchtab1[k,1]=ifelse(is.na(matched)==TRUE, NA, all.tissues[k])
+            matchtab1[k,2]=ifelse(is.na(matched)==TRUE, 0, 1)
+            
+          }
+          
+          matchtab2[[j]]=matchtab1
+          #print(matchtab2)
+        }
+        
+        all.matches[[t]]=matchtab2
+        #print(all.matches)
+      
+     
+#-------------------lond--cis------------------------       
+      }else{
+        
+        matchtab2=list()
+        
+        for(j in 1:length(londM1$type1)){
+          
+          index=(londM1$type1[j]-1)*3+(1:3)
+          members=colnames(trioData[,index])[3]
+          print(members)
+          
+          for(k in 1:length(all.tissues)){
+            
+            trioData2=colnames(loaded.datasets[[k]])
+            
+            matched=match(members, trioData2)
+            #print(matched)
+            
+            matchtab1[k,1]=ifelse(is.na(matched)==TRUE, NA, all.tissues[k])
+            matchtab1[k,2]=ifelse(is.na(matched)==TRUE, 0, 1)
+            
+          }
+          
+          matchtab2[[j]]=matchtab1
+          #print(matchtab2)
+        }
+        
+        all.matches[[t]]=matchtab2
+        #print(all.matches)
+        
+      }
       
     }
-    
-    #extract tissues the trio was found in and its Mtype for each tissue
-    indicator=isTRUE(all.equal(logivec, not_in_tiss, check.attributes=FALSE))
-    
-    if(verbose==TRUE & indicator==FALSE){
-      print(paste("Found a Match in", tissues.vec[t], "!", sep = " "))
-      }
-    
-    data1[t,1]=ifelse(isTRUE(indicator)==TRUE, NA, tissues.vec[t])
-    
-    data1[t,2]=ifelse(is.na(match(TRUE, logivec))==TRUE, NA, Mtypes[match(TRUE,logivec)] )
-    
 
-  }
+  } #closes first 4loop
   
-  #printing
-  if(verbose==TRUE){
-    print(summary(as.factor(data1$Model)))
-    print(paste("Trio found in ",length(na.omit(data1$Tissue)), " Tissues", sep = ""))
-    
-  }
+  names(all.matches)=tissues.vec
   
+  if(FDR=="ADDIS" & mediator == "trans"){names(all.matches[[1]])=as.character(addisM1$type2)}
+  if(FDR=="ADDIS" & mediator == "cis"){names(all.matches[[1]])=as.character(addisM1$type1)}
+  if(FDR=="LOND" & mediator == "trans"){names(all.matches[[1]])=as.character(londM1$type2)}
+  if(FDR=="LOND" & mediator == "cis"){names(all.matches[[1]])=as.character(londM1$type1)}
   
-  return(data1)
+  return(all.matches)
   
-}
+} #closes func
 
 
 #===========================================================================================================
@@ -86,7 +214,7 @@ uncover_net=function(trio_number=NULL, FDR="ADDIS", verbose=TRUE){
 
 
 
-
+#source("/mnt/ceph/jarredk/Reg_Net/AL_regnet.R")
 
 
 

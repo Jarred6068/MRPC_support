14997c677ea978091b59504f0a3c39fa16111000
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2021-05-26 16:32:49 -0700

    various edits

--- Methyl/calcCOR.R
+++ Methyl/calcCOR.R
@@ -17,7 +17,7 @@ triobuildlist=loadRData(fileName = "/mnt/ceph/jarredk/Methyl/triobuildlist.Rdata
 
 
 
-
+#function to compute correlations between SNPS 
 
 
 calc.cors=function(triolist=NULL, genomat=NULL){
@@ -26,6 +26,8 @@ calc.cors=function(triolist=NULL, genomat=NULL){
   for(i in 1:length(triobuildlist)){size1[i]=length(triobuildlist[[i]])}
   
   print(paste("number of trios to compute", paste0(sum(vec1),"X",dim(genos.mat)[2]), sep = ":"))
+  
+  
   
 }
 

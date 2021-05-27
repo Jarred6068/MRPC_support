9b89ef539614252689acf7de451c3309227b64a9
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2021-05-27 13:50:03 -0700

    additional coding added to calccor

--- Methyl/calcCOR.R
+++ Methyl/calcCOR.R
@@ -8,16 +8,76 @@ loadRData <- function(fileName=NULL){
 #load in necesarry files from workspace
 
 load(file="/mnt/ceph/jarredk/Methyl/Wksp1.Rdata")
-
-genos.mat=loadRData(fileName = "/mnt/ceph/jarredk/Methyl/fakegenos.Rdata")
-
+genos.mat=loadRData(fileName = "/mnt/ceph/jarredk/Methyl/fakegenosBIG.Rdata")
 EM.triolist=loadRData(fileName = "/mnt/ceph/jarredk/Methyl/completedTrios1.Rdata")
-
 triobuildlist=loadRData(fileName = "/mnt/ceph/jarredk/Methyl/triobuildlist.Rdata")
+#meta_G=read.delim(file="/mnt/ceph/megheib/M_G_data/GPL10558_GE_R2.txt", sep="")
+meta_M=read.csv(file = "/mnt/ceph/megheib/M_G_data/GPL13534_M.csv")
+
+imp.meta_M=meta_M[,c(1,12,13)]
+
+#simulate the chromosome and position of each SNP
+
+sim.chr=sample(c(1:22,"X","Y"), dim(genos.mat)[2], replace = TRUE)
+
+simu.pos=function(genosmat=NULL, chr.vec=NULL){
+  
+  #pos=matrix(0, nrow = dim(genosmat)[2], ncol = 2)
+  pos=NULL
+  chr.sizes=read.csv(file = "/mnt/ceph/jarredk/Methyl/chrsizes.csv")
+  
+  for(i in 1:length(chr.vec)){
+    
+    idx=match(chr.vec[i],chr.sizes[,1])
+    start.pos=sample(c(1000000:(chr.sizes[idx,2]-1000000)), 1)
+    #end.pos=start.pos+1000000
+    #print(start.pos)
+    #print(end.pos)
+    pos[i]=start.pos
+    
+  }
+  
+  return(simu.pos)
+  
+}
+
+sim.chrpos=simu.pos(genosmat = genos.mat, chr.vec = sim.chr)
+simulated.meta=cbind.data.frame(sim.chr, simu.pos)
+
+#save(simulated.meta, file = "/mnt/ceph/jarredk/Methyl/fakegenoMeta.Rdata")
+simulated.meta=loadRData(fileName = "/mnt/ceph/jarredk/Methyl/fakegenoMeta.Rdata")
+
+#acquire the "in-use" probe ID's from the completed trio list
+
+probenames=list()
+cn=function(X){ v=colnames(X)[2]; return(v)}
+
+for(i in 1:length(EM.triolist)){
+  
+  if(is.null(unlist(lapply(EM.triolist[[i]], cn)))){
+    
+    probenames[[i]]=NA
+    
+  }else{
+    
+    probenames[[i]]=unlist(lapply(EM.triolist[[i]], cn))
+    
+  }
+}
+
+Mprobenames.final=na.omit(unlist(probenames))
+
+#save(Mprobenames.final, file = "/mnt/ceph/jarredk/Methyl/Used.Mprobes.final.Rdata")
+Mprobenames.final=loadRData(fileName = "/mnt/ceph/jarredk/Methyl/Used.Mprobes.final.Rdata")
 
 
+#retreive relevant metadata for the "In-Use" methylation probes:
 
-#function to compute correlations between SNPS 
+matched.meta_M=match(Mprobenames.final, imp.meta_M[,1])
+imp.meta_M.final=imp.meta_M[matched.meta_M,c(2:3)]
+
+
+#function to compute correlations between SNPS based on locations within each chromosome
 
 
 calc.cors=function(triolist=NULL, genomat=NULL){

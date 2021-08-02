07d03964cde36b82d476314c01aadd2f8786e43d
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2021-08-02 16:20:35 -0700

    write all tissue meta files to master table with multi sheet

---
+++ Manuscript/write_multisheet.R
@@ -0,0 +1,37 @@
+
+
+library('writexl', lib="/mnt/ceph/jarredk/Rpackages")
+
+tiss=read.csv("/mnt/ceph/jarredk/AddisReRunFiles/tissuenames.csv")
+
+AM1T1=vector("list", 48)
+AM1T2=vector("list", 48)
+LM1T1=vector("list", 48)
+LM1T2=vector("list", 48)
+
+names(AM1T1)=tiss[,2]
+names(AM1T2)=tiss[,2]
+names(LM1T1)=tiss[,2]
+names(LM1T2)=tiss[,2]
+
+for(i in 1:dim(tiss)[1]){
+  
+  AM1T1[[i]]=as.data.frame(read.csv(file = paste0("/mnt/ceph/jarredk/Reg_Net/AM1T1/",tiss[i,2],".csv")))[,-1]
+  AM1T2[[i]]=as.data.frame(read.csv(file = paste0("/mnt/ceph/jarredk/Reg_Net/AM1T2/",tiss[i,2],".csv")))[,-1]
+  LM1T1[[i]]=as.data.frame(read.csv(file = paste0("/mnt/ceph/jarredk/Reg_Net/LM1T1/",tiss[i,2],".csv")))[,-1]
+  LM1T2[[i]]=as.data.frame(read.csv(file = paste0("/mnt/ceph/jarredk/Reg_Net/LM1T2/",tiss[i,2],".csv")))[,-1]
+  
+  
+}
+
+
+write_xlsx(AM1T1, path = "/mnt/ceph/jarredk/Manuscript/AM1T1_master.xlsx")
+write_xlsx(AM1T2, path = "/mnt/ceph/jarredk/Manuscript/AM1T2_master.xlsx")
+write_xlsx(LM1T1, path = "/mnt/ceph/jarredk/Manuscript/LM1T1_master.xlsx")
+write_xlsx(LM1T2, path = "/mnt/ceph/jarredk/Manuscript/LM1T2_master.xlsx")
+
+
+
+
+
+

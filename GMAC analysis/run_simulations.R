7bd28a6dbb1fe17d71f9097b36d26074cf143860
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2021-11-01 15:13:50 -0700

    updates 11/1/2021
    Changed run simulations to run on all trios of a given tissue

--- GMAC analysis/run_simulations.R
+++ GMAC analysis/run_simulations.R
@@ -9,32 +9,40 @@
 source("/mnt/ceph/jarredk/GMACanalysis/GMACpostproc.R")
 
 
-nn=50
+#nn=50
 
 #small truth model large inferred model
-for(i in 1:length(tissues.vec[,1])){
+#for(i in 1:length(tissues.vec[,1])){
   
-  print(tissues.vec[i,1])
-  l1=cross.analyze(tissues=tissues.vec[i,1], save=FALSE)
-  all.m0m3=l1$final.tables[[1]][c(which(l1$final.tables[[1]]$Addis.Class=="M3"), which(l1$final.tables[[1]]$Addis.Class=="MO")),]
-  print(head(all.m0m3[,1:6]))
-  trios=sample(all.m0m3$Trio.Num, nn)
+  # print(tissues.vec[i,1])
+   l1=cross.analyze(tissues="AdiposeSubcutaneous", save=FALSE)
+  # all.m0m3=l1$final.tables[[1]][c(which(l1$final.tables[[1]]$Addis.Class=="M3"), which(l1$final.tables[[1]]$Addis.Class=="MO")),]
+  # print(head(all.m0m3[,1:6]))
+  # trios=sample(all.m0m3$Trio.Num, nn)
+   trios=l1$final.tables[[1]]$Trio.Num
+   med.types=l1$final.tables[[1]]$Mediation.type
   print("running Simulations...")
-  ot1=run.simu12(tissue = tissues.vec[i,1] ,trios=trios, 
-                 l1.table=all.m0m3,
-                 mod.type.vec=all.m0m3$Mediation.type[match(trios, all.m0m3$Trio.Num)],
+  # ot1=run.simu12(tissue = "AdiposeSubcutaneous" ,trios=trios, 
+  #                l1.table=all.m0m3,
+  #                mod.type.vec=all.m0m3$Mediation.type[match(trios, all.m0m3$Trio.Num)],
+  #                alpha=0.001, n="random")
+  # 
+  ot1=run.simu12(tissue = "AdiposeSubcutaneous" ,trios=trios, 
+                 l1.table=l1$final.tables[[1]],
+                 mod.type.vec=med.types,
                  alpha=0.001, n="random")
   print("...done")
-  ot1$Tissue=rep(tissues.vec[i,1])
+  #ot1$Tissue=rep(tissues.vec[i,1])
+  ot1$Tissue=rep("AdiposeSubcutaneous")
   
-  if(i == 1){
-    write.table(ot1, file = "/mnt/ceph/jarredk/GMACanalysis/master_tables/simulations.txt", col.names = T, row.names = F, sep = "\t", 
+  #if(i == 1){
+    write.table(ot1, file = "/mnt/ceph/jarredk/GMACanalysis/master_tables/simulations_adipose_subcutaneous.txt", col.names = T, row.names = F, sep = "\t", 
                 quote=F)
-  }else{
-    write.table(ot1, file = "/mnt/ceph/jarredk/GMACanalysis/master_tables/simulations.txt", col.names = F, row.names = F, sep = "\t",
-                quote=F, append = T)
-  }
+  #}else{
+  #  write.table(ot1, file = "/mnt/ceph/jarredk/GMACanalysis/master_tables/simulations_wholeblood.txt", col.names = F, row.names = F, sep = "\t",
+  #              quote=F, append = T)
+  #}
   
-}
+#}
 
 
--- GMAC analysis/scrapcode.R
+++ GMAC analysis/scrapcode.R
@@ -33,15 +33,17 @@ MRPC.fit.FDR.addis <- MRPC(test.data,
 as(MRPC.fit.FDR.addis@graph, "matrix")[1:3,1:3]
 
 
-Lond2Addis.lookup(trio.index=17, tissue.name="WholeBlood", with.pc=TRUE)
+Lond2Addis.lookup(trio.index=133, tissue.name="WholeBlood", with.pc=TRUE)
 
-list.data=cross.regress(tissue="WholeBlood", trio.ind=17, mod.type="cis", addis.pcs=NULL)
+list.data=cross.regress(tissue="WholeBlood", trio.ind=8112, mod.type="cis", addis.pcs=NULL)
 
 
 fname1=paste0("/mnt/ceph/jarredk/GMACanalysis/GMAC.permutedREG.trio",
-              8667,".png")
-out.permreg=run.permuted.reg(trio=list.data$GMAC, nperms=10000, plot=TRUE, filename=fname1)
-
+              8112,".png")
+start.t=Sys.time()
+out.permreg=run.permuted.reg(trio=list.data$GMAC, nperms=1000, plot=F, filename=fname1)
+end.t=Sys.time()
+end.t-start.t
 
 #========================================================================================
 ##suppression analysis
@@ -77,13 +79,13 @@ nn=10
 l1=cross.analyze(tissues="WholeBlood", save=FALSE)
 all.m0m3=l1$final.tables[[1]][c(which(l1$final.tables[[1]]$Addis.Class=="M3"), which(l1$final.tables[[1]]$Addis.Class=="MO")),]
 trios=sample(all.m0m3$Trio.Num, nn)
-ot1=run.simu12(tissue = "WholeBlood" ,trios=trios, 
+ot1=run.simu12(tissue = "WholeBlood" ,trios=trios, l1.table=all.m0m3,
                mod.type.vec=all.m0m3$Mediation.type[match(trios, all.m0m3$Trio.Num)],
                alpha=0.001, n="random", seed=NULL)
 
+run.permuted.reg(list.data$addis, nperms=1000, plot=FALSE, filename=NULL, Alg="ADDIS")$p.value
 
-
-list.data=cross.regress(tissue="WholeBlood", trio.ind=133, mod.type="cis", addis.pcs=NULL)
+list.data=cross.regress(tissue="AdiposeSubcutaneous", trio.ind=5683, mod.type="cis", addis.pcs=NULL)
 x=simu2(tissue = "WholeBlood", data=list.data$GMAC, seed=NULL, mod.type="Both", n=10, verbose=TRUE)
 xx=simu1(data=list.data$GMAC,alpha=0.001, mod.type="Both", verbose=TRUE)
 
@@ -98,10 +100,11 @@ for(i in 1:length(tissues.vec[,1])){
 
 
 
+run.permuted.reg(trio=list.data$GMAC, nperms=1000, plot=TRUE, filename="/mnt/ceph/jarredk/GMACanalysis/trio5683ASpermuted.png", Alg="GMAC")
 
 
-
-
+check.ns.gmac(input.list.data=list.data, iters=1, alpha=0.01, print.it=FALSE, which.mod="GMAC",
+                       plot.it=T, save.name="TEST5683AS_CHECK1")
 
 
 #looking at methyl cors
@@ -141,16 +144,16 @@ print(sum(V))
 #checking trios with conflicting mediation and permutation p values
 #must be run post run_simulations.slurm on cluster
 source("/mnt/ceph/jarredk/GMACanalysis/GMACpostproc.R")
-sim=read.table(file="/mnt/ceph/jarredk/GMACanalysis/master_tables/simulations.txt", sep="\t", header = T)
+sim=read.table(file="/mnt/ceph/jarredk/GMACanalysis/master_tables/simulations_wholeblood.txt", sep="\t", header = T)
 
-wt=sim[which(sim$Med.pvalue.GMAC>0.05),]
+#wt=sim[which(sim$Med.pvalue.GMAC>0.05),]
 
-snp.dist=as.data.frame(matrix(0, nrow = dim(wt)[1], ncol = 3))
+snp.dist=as.data.frame(matrix(0, nrow = dim(sim)[1], ncol = 3))
 colnames(snp.dist)=c("hr","het","ha")
 
-for(i in 1:dim(wt)[1]){
+for(i in 1:dim(sim)[1]){
   
-  if(wt$Med.type[i]=="Trans.Med"){
+  if(sim$Med.type[i]=="Trans.Med"){
     list.data=cross.regress(tissue=wt$Tissue[i], trio.ind=wt$Trio.Num[i], mod.type="trans", addis.pcs=NULL, verbose=F)
   }else{
     list.data=cross.regress(tissue=wt$Tissue[i], trio.ind=wt$Trio.Num[i], mod.type="cis", addis.pcs=NULL, verbose=F)
@@ -161,9 +164,90 @@ for(i in 1:dim(wt)[1]){
 }
 
 
-output1=cbind.data.frame(wt, snp.dist)
+output1=cbind.data.frame(sim, snp.dist)
 
-write.csv(output1, file = "/mnt/ceph/jarredk/GMACanalysis/master_tables/TRIOS_imbalanced_genotypes.csv", row.names = F)
+write.csv(output1, file = "/mnt/ceph/jarredk/GMACanalysis/master_tables/TRIOS_imbalanced_genotypes_WB.csv", row.names = F)
+
+
+
+
+
+
+
+#Add in simulation for MRPC on GMAC model to the simulations.txt dataset
+source("/mnt/ceph/jarredk/GMACanalysis/GMACpostproc.R")
+sim=read.table(file="/mnt/ceph/jarredk/GMACanalysis/master_tables/simulations_wholeblood.txt", sep="\t", header = T)
+
+p=NULL
+
+for(i in 1:dim(sim)[1]){
+  print(i)
+  
+  L2A=Lond2Addis.lookup(trio.index=sim$Trio.Num[i], tissue.name=sim$Tissue[i], with.pc=TRUE)
+  
+  if(length(colnames(L2A$correlation))>3){
+    addis.pcs=colnames(L2A$correlation)[-c(1:3)]
+  }else{
+    addis.pcs=NULL
+  }
+  
+  list.data=cross.regress(tissue=sim$Tissue[i], trio.ind=sim$Trio.Num[i], mod.type=sim$Med.type[i], addis.pcs=addis.pcs, verbose=F)
+  
+  p[i]=simu3(MRPC.data=list.data$addis, GMAC.data=list.data$GMAC, mod.type=sim$Med.type[i], verbose=F)$pvalue
+  
+  
+}
+
+sim$TGM.p=p
+
+write.table(sim, file = "/mnt/ceph/jarredk/GMACanalysis/master_tables/simulations_wholeblood.txt", col.names = T, row.names = F, sep = "\t",
+            quote=F)
+
+
+
+
+
+
+
+
+#testing GMAC inference on trios with rare alleles in cis-eQTL:
+source("/mnt/ceph/jarredk/GMACanalysis/GMACanalysis.R")
+library('GMAC', lib='/mnt/ceph/jarredk/Rpackages')
+library('mice', lib='/mnt/ceph/jarredk/Rpackages')
+library('missMDA', lib='/mnt/ceph/jarredk/Rpackages')
+sim.odd=read.csv(file="mnt/ceph/jarredk/GMACanalysis/master_tables/TRIOS_imbalanced_genotypes.csv", header = T)
+#trios.to.keep=
+t=1
+
+#load additional files
+file1=paste("/mnt/lfs2/mdbadsha/peer_example/SNP_cis_trans_files/GTEx_version_8/",
+            tissues.vec[t,1],"_AllPC/data.snp.cis.trans.final.",
+            tissues.vec[t,1],".V8.unique.snps.RData", sep = "")
+
+file2=paste("/mnt/lfs2/mdbadsha/peer_example/SNP_cis_trans_files/GTEx_version_8/",
+            tissues.vec[t,1],"_AllPC/PCs.matrix.",
+            tissues.vec[t,1], ".RData", sep="")
+
+#load trio, pc, and confounders data
+trios=loadRData(fileName=file1)
+PCs=loadRData(fileName=file2)
+#sig.asso.pcs=loadRData(fileName=file3)
+#edata=loadRData(fileName=file4)
+confounders=read.table(paste0("/mnt/lfs2/mdbadsha/peer_example/SNP_cis_trans_files/GTEx_version_8/GTEx_Analysis_v8_eQTL_covariates/", 
+                              tissues.vec[t,2], '.v8.covariates.txt'), header = T, sep="\t", row.names = 1)[66:68,]
+
+print(paste('data loaded for', tissues.vec[t,2], sep = ' '))
+
+#run assemble tables function
+tables.gmac.list=assemble.tables(trio.table=trios, PCmat = PCs, kc = confounders, which.imp = 'miss')
+
+output <- gmac(known.conf = tables.gmac.list$known.conf, cov.pool = tables.gmac.list$cov.pool, 
+               exp.dat = tables.gmac.list$exp.dat[row.match(sim.odd[1,18:19], matrix(row.names(tables.gmac.list$exp.dat)), ncol=2, byrow=T ),],
+               snp.dat.cis = tables.gmac.list$snp.dat.cis[5863,], 
+               trios.idx = tables.gmac.list$trios.idx[5683,], nperm = 10000, nominal.p = TRUE)
+
+
+
 
 
 

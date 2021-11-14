45ba516ec76bd44ed0781938f04a9feee6ece204
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2021-11-13 22:16:15 -0800

    Update create_fake_genodata.R
    fixed it to allow for other and counted alleles

--- Methyl/create_fake_genodata.R
+++ Methyl/create_fake_genodata.R
@@ -1,6 +1,9 @@
 
 set.seed(123)
 
+n=100
+m=1000
+
 fake_genos=function(csize=NULL, rsize=NULL){
   
   genos=sample(c(0,1,2), csize*rsize, replace = TRUE)
@@ -11,22 +14,22 @@ fake_genos=function(csize=NULL, rsize=NULL){
   
 }
 
-genos.mat=fake_genos(4000000, 1000)
+genos.mat=fake_genos(n, m)
 
 genos.mat[1:10,1:5]
 
 sim.chr=sample(c(1:22,"X","Y"), dim(genos.mat)[2], replace = TRUE)
 
-simu.pos=function(genosmat=NULL, chr.vec=NULL){
+simu.pos=function(genosmat=NULL, chr.vec=NULL, q=n){
   
   #pos=matrix(0, nrow = dim(genosmat)[2], ncol = 2)
-  pos=rep(0, 4000000)
+  pos=rep(0, q)
   chr.sizes=read.csv(file = "/mnt/ceph/jarredk/Methyl/chrsizes.csv")
   
   for(i in 1:length(chr.vec)){
     
     idx=match(chr.vec[i],chr.sizes[,1])
-    start.pos=runif(1, 1000000, chr.sizes[idx,2]-1000000)
+    start.pos=round(runif(1, 1000000, chr.sizes[idx,2]-1000000))
     #end.pos=start.pos+1000000
     #print(start.pos)
     #print(end.pos)
@@ -37,14 +40,22 @@ simu.pos=function(genosmat=NULL, chr.vec=NULL){
   return(pos)
   
 }
+possible.alleles=c("A","T","C","G")
 
-names1=paste0("SNP","_",c(1:4000000),"_chr", sim.chr)
+sim.other=sample(possible.alleles, dim(genos.mat)[2], replace = TRUE)
+sim.counted=NULL
+
+fn=function(x,v){x=sample(v[-which(x==v)], 1, replace = T )}
+
+sim.counted=unlist(lapply(as.list(sim.other), fn, possible.alleles))
 
 sim.chrpos=simu.pos(genosmat = genos.mat, chr.vec = sim.chr)
-simulated.meta=cbind.data.frame(names1, sim.chr, sim.chrpos)
+
+names1=paste0("SNP","_",sim.chrpos,"_chr", sim.chr)
+simulated.meta=cbind.data.frame(names1, sim.chr, sim.chrpos, sim.other, sim.counted)
 
 #colnames(genos.mat)=names1
-colnames(simulated.meta)=c("SNP_ID","CHR","Coordinate")
+colnames(simulated.meta)=c("SNP_ID","chr","coordinate", "OtherAllele", "CountedAllele")
 
 genos.mat=t(genos.mat)
 genos.mat.new=cbind.data.frame(names1, genos.mat)
@@ -53,7 +64,13 @@ colnames(genos.mat.new)=c("SNP_ID", paste0("bsgs_", c(1:1000)))
 genos.mat.new[1:10,1:5]
 sim.chr[1:5]
 
-save(simulated.meta, file = "/mnt/ceph/jarredk/Methyl/fakegenoMeta_2.Rdata")
+#save(simulated.meta, file = "/mnt/ceph/jarredk/Methyl/fakegenoMeta_2.Rdata")
+write.table(simulated.meta, file = "/mnt/ceph/jarredk/Methyl/CC_all/Correlation_CalculationV5_Scott/Input/Example_Genotype_Metadata_small.txt", 
+            col.names = T, sep = "\t", quote = F, row.names = F)
+
 #simulated.meta=loadRData(fileName = "/mnt/ceph/jarredk/Methyl/fakegenoMeta.Rdata")
 
-save(genos.mat.new, file = "/mnt/ceph/jarredk/Methyl/fakegenosBIG_2.Rdata")
+#save(genos.mat.new, file = "/mnt/ceph/jarredk/Methyl/fakegenosBIG_2.Rdata")
+
+write.table(genos.mat.new, file = "/mnt/ceph/jarredk/Methyl/CC_all/Correlation_CalculationV5_Scott/Input/Example_Genotype_data_small.txt", 
+            col.names = T, sep = "\t", quote = F, row.names = F)

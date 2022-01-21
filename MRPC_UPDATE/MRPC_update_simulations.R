470ba893623e094146b7807bdae64696ee006dd7
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2022-01-21 12:55:20 -0800

    Update MRPC_update_simulations.R

--- MRPC_UPDATE/MRPC_update_simulations.R
+++ MRPC_UPDATE/MRPC_update_simulations.R
@@ -10,16 +10,6 @@ library('qvalue', lib="/mnt/ceph/jarredk/Rpackages")
 
 source("/mnt/ceph/jarredk/MRPC_UPDATE/MRPCgeneral.R")
 source("/mnt/ceph/jarredk/GMACanalysis/GMACpostproc.R")
-l1=cross.analyze(tissues="AdiposeSubcutaneous", save=FALSE)
 
-trio.list=list()
-for(i in 1:25){
-  
-  list.data=cross.regress(tissue="AdiposeSubcutaneous", trio.ind=l1$final.tables[[1]]$Trio.Num[i], mod.type="both", addis.pcs=NULL, verbose=F)
-  data=list.data$GMAC[, -((dim(list.data$GMAC)[2]-2):dim(list.data$GMAC)[2])]
-  data=data[,c(3,2,1, 4:dim(data)[2])]
-  trio.list[[i]]=as.data.frame(data)
-}
 
-m=1000
-out.coeffs=lapply(trio.list, reg.with.variant, permuted=TRUE, nperms=m, return.indicator=TRUE, alpha=0.01)
+simulate

eb1a3023c39d3baf5f388f4fd1f3a68f0a831570
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2022-03-25 12:27:17 -0700

    Update scrapcode.R

--- GMAC analysis/scrapcode.R
+++ GMAC analysis/scrapcode.R
@@ -32,7 +32,7 @@ MRPC.fit.FDR.addis <- MRPC(test.data,
 
 as(MRPC.fit.FDR.addis@graph, "matrix")[1:3,1:3]
 
-Lond2Addis.lookup(trio.index=54, tissue.name="AdiposeSubcutaneous", with.pc=TRUE)
+data1=Lond2Addis.lookup(trio.index=54, tissue.name="AdiposeSubcutaneous", with.pc=TRUE)
 
 list.data=cross.regress(tissue="AdiposeSubcutaneous", trio.ind=54, mod.type="both", addis.pcs=NULL)
 
@@ -96,7 +96,7 @@ x=simu2(tissue = "WholeBlood", data=list.data$GMAC, seed=NULL, mod.type="Both", 
 #generate table
 for(i in 1:length(tissues.vec[,1])){
   l1=cross.analyze(tissues=tissues, save=FALSE)
-  
+
 }
 
 
@@ -112,13 +112,13 @@ check.ns.gmac(input.list.data=list.data, iters=1, alpha=0.01, print.it=FALSE, wh
 #looking at methyl cors
 V=NULL
 for(i in c(1:22, "X", "Y")){
-  A=read.table(file=paste0("/mnt/ceph/jarredk/Methyl/BSGS_Correlations/BSGS_Correlations_20210914/M_chr", 
+  A=read.table(file=paste0("/mnt/ceph/jarredk/Methyl/BSGS_Correlations/BSGS_Correlations_20210914/M_chr",
                            i,"_correlations.txt"), sep="\t", header=T)
   #print(paste0("chr",i,"=",length(unique(A$Methyl_Probe_ID[which(is.na(A$Cor))]))))
   print(paste0("Unique Probes for chr",i,"=",length(unique(A$Methyl_Probe_ID))))
-  
+
   V[i]=length(unique(A$Methyl_Probe_ID))
-  
+
 }
 
 print(sum(V))
@@ -140,21 +140,21 @@ p=NULL
 
 for(i in 1:dim(sim)[1]){
   print(i)
-  
+
   L2A=Lond2Addis.lookup(trio.index=sim$Trio.Num[i], tissue.name=sim$Tissue[i], with.pc=TRUE)
-  
+
   if(length(colnames(L2A$correlation))>3){
     addis.pcs=colnames(L2A$correlation)[-c(1:3)]
   }else{
     addis.pcs=NULL
   }
-  
+
   list.data=cross.regress(tissue=sim$Tissue[i], trio.ind=sim$Trio.Num[i], mod.type=sim$Med.type[i], addis.pcs=addis.pcs, verbose=F)
-  
+
   out3=simu3(MRPC.data=list.data$addis, GMAC.data=list.data$GMAC, mod.type=sim$Med.type[i], verbose=F)
-  
+
   p[i]=out3$pvalue
-  
+
 }
 
 sim$TGM.p=p
@@ -181,17 +181,17 @@ snp.dist=as.data.frame(matrix(0, nrow = dim(sim)[1], ncol = 3))
 colnames(snp.dist)=c("hr","het","ha")
 
 for(i in 1:dim(sim)[1]){
-  
+
   if(sim$Med.type[i]=="Trans.Med"){
     list.data=cross.regress(tissue=sim$Tissue[i], trio.ind=sim$Trio.Num[i], mod.type="trans", addis.pcs=NULL, verbose=F)
   }else{
     list.data=cross.regress(tissue=sim$Tissue[i], trio.ind=sim$Trio.Num[i], mod.type="cis", addis.pcs=NULL, verbose=F)
   }
-  
-  
+
+
   #account for SNPs with less than 3 genotypes
   if(length(summary(factor(list.data$GMAC$SNP)))<3){
-    
+
     if( length(which(names(summary(factor(list.data$GMAC$SNP)))=="0"))==0 ){
       snp.dist[i,]=c(NA ,summary(factor(list.data$GMAC$SNP)))
     }else if ( length(which(names(summary(factor(list.data$GMAC$SNP)))=="1"))==0 ){
@@ -199,13 +199,13 @@ for(i in 1:dim(sim)[1]){
     }else{
       snp.dist[i,]=c(summary(factor(list.data$GMAC$SNP)), NA)
     }
-    
+
   }else{
-    
+
     snp.dist[i,]=summary(factor(list.data$GMAC$SNP))
-    
+
   }
-  
+
 }
 
 
@@ -227,20 +227,20 @@ p=NULL
 
 for(i in 1:dim(sim)[1]){
   print(i)
-  
+
   L2A=Lond2Addis.lookup(trio.index=sim$Trio.Num[i], tissue.name=sim$Tissue[i], with.pc=TRUE)
-  
+
   if(length(colnames(L2A$correlation))>3){
     addis.pcs=colnames(L2A$correlation)[-c(1:3)]
   }else{
     addis.pcs=NULL
   }
-  
+
   list.data=cross.regress(tissue=sim$Tissue[i], trio.ind=sim$Trio.Num[i], mod.type=sim$Med.type[i], addis.pcs=addis.pcs, verbose=F)
-  
+
   p[i]=simu3(MRPC.data=list.data$addis, GMAC.data=list.data$GMAC, mod.type=sim$Med.type[i], verbose=F)$pvalue
-  
-  
+
+
 }
 
 sim$TGM.p=p
@@ -262,7 +262,7 @@ p=NULL
 
 for(i in 1:dim(t1)[1]){
   #print(i)
-  
+
   L2A=Lond2Addis.lookup(trio.index=t1$Trio.Num[i], tissue.name=t1$Tissue[i], with.pc=TRUE)
   print(dim(L2A$correlation))
   if(length(colnames(L2A$correlation))>3){
@@ -270,14 +270,14 @@ for(i in 1:dim(t1)[1]){
   }else{
     addis.pcs=NULL
   }
-  
+
   list.data=cross.regress(tissue=t1$Tissue[i], trio.ind=t1$Trio.Num[i], mod.type=t1$Med.type[i], addis.pcs=addis.pcs, verbose=F)
-  
+
   out=simu3(MRPC.data=list.data$addis, GMAC.data=list.data$GMAC, mod.type=t1$Med.type[i], verbose=F)
   print(dim(out$sim.data))
   p[i]=run.permuted.reg(out$sim.data, nperms=1000, Alg="ADDIS", med.type = t1$Med.type[i])$p.value
   print(p[i])
-  
+
 }
 
 
@@ -335,7 +335,7 @@ trios=loadRData(fileName=file1)
 PCs=loadRData(fileName=file2)
 #sig.asso.pcs=loadRData(fileName=file3)
 #edata=loadRData(fileName=file4)
-confounders=read.table(paste0("/mnt/lfs2/mdbadsha/peer_example/SNP_cis_trans_files/GTEx_version_8/GTEx_Analysis_v8_eQTL_covariates/", 
+confounders=read.table(paste0("/mnt/lfs2/mdbadsha/peer_example/SNP_cis_trans_files/GTEx_version_8/GTEx_Analysis_v8_eQTL_covariates/",
                               tissues.vec[t,2], '.v8.covariates.txt'), header = T, sep="\t", row.names = 1)[66:68,]
 
 print(paste('data loaded for', tissues.vec[t,2], sep = ' '))
@@ -343,9 +343,9 @@ print(paste('data loaded for', tissues.vec[t,2], sep = ' '))
 #run assemble tables function
 tables.gmac.list=assemble.tables(trio.table=trios, PCmat = PCs, kc = confounders, which.imp = 'miss')
 
-output <- gmac(known.conf = tables.gmac.list$known.conf, cov.pool = tables.gmac.list$cov.pool, 
+output <- gmac(known.conf = tables.gmac.list$known.conf, cov.pool = tables.gmac.list$cov.pool,
                exp.dat = tables.gmac.list$exp.dat[row.match(sim.odd[1,18:19], matrix(row.names(tables.gmac.list$exp.dat)), ncol=2, byrow=T ),],
-               snp.dat.cis = tables.gmac.list$snp.dat.cis[5863,], 
+               snp.dat.cis = tables.gmac.list$snp.dat.cis[5863,],
                trios.idx = tables.gmac.list$trios.idx[5683,], nperm = 10000, nominal.p = TRUE)
 
 
@@ -384,7 +384,7 @@ med.types=l1$final.tables[[1]]$Mediation.type
 print("running Simulations...")
 
 start.time=Sys.time()
-ot1=run.simu12(tissue = "WholeBlood" ,trios=trios[1:10], 
+ot1=run.simu12(tissue = "WholeBlood" ,trios=trios[1:10],
                l1.table=l1$final.tables[[1]][1:10,],
                mod.type.vec=med.types[1:10],
                alpha=0.001, n="random")
@@ -444,7 +444,7 @@ source("/mnt/ceph/jarredk/GMACanalysis/GMACpostproc.R")
 
 tissue="WholeBlood"
 l1=cross.analyze(tissues=tissue, save=FALSE)
-#pull out trios which were no longer significant after the GSS simulation 
+#pull out trios which were no longer significant after the GSS simulation
 #trios=sim$Trio.Num[sim$Nominal.p.GSS>0.05][1:5]
 #mod.type.vec=sim$Med.type[sim$Nominal.p.GSS>0.05][1:5]
 trios = sim$Trio.Num
@@ -459,44 +459,44 @@ TGM.median.p=NULL
 GSS.median.p=NULL
 
 for(i in 1:5){
-  
-  
+
+
   print(paste0("getting data for trio # ", trios[i]))
   if(mod.type.vec[i]=="Both"){
-    
-    list.data=cross.regress(tissue=tissue, 
-                            trio.ind=356, 
-                            mod.type="cis", 
-                            #addis.pcs=addis.pcs, 
+
+    list.data=cross.regress(tissue=tissue,
+                            trio.ind=356,
+                            mod.type="cis",
+                            #addis.pcs=addis.pcs,
                             verbose = TRUE)
-    
+
   }else if(mod.type.vec[i]=="Cis.Med"){
-    
-    list.data=cross.regress(tissue=tissue, 
-                            trio.ind=trios[i], 
-                            mod.type="cis", 
-                            #addis.pcs=addis.pcs, 
+
+    list.data=cross.regress(tissue=tissue,
+                            trio.ind=trios[i],
+                            mod.type="cis",
+                            #addis.pcs=addis.pcs,
                             verbose = FALSE)
   }else{
-    
-    list.data=cross.regress(tissue=tissue, 
-                            trio.ind=trios[i], 
-                            mod.type="trans", 
-                            #addis.pcs=addis.pcs, 
+
+    list.data=cross.regress(tissue=tissue,
+                            trio.ind=trios[i],
+                            mod.type="trans",
+                            #addis.pcs=addis.pcs,
                             verbose = FALSE)
-    
+
   }
   print("...done...")
-  
+
   p1=as.data.frame(matrix(0, nrow = ss, ncol = 4))
   b1=as.data.frame(matrix(0, nrow = ss, ncol = 4))
   colnames(p1)=c("simu1.p", "simu2.p", "simu3.p", "simu4.p")
   colnames(b1)=c("simu1.b", "simu2.b", "simu3.b", "simu4.b")
-  
+
   print(paste0("running all simulations for trio # ", trios[i]))
   for(j in 1:ss){
-    
-    
+
+
     out=simu1(list.data$GMAC, alpha = 0.001, mod.type=mod.type.vec[i], verbose=F)
     nn=floor(runif(1, 1, 20))
     #print(nn)
@@ -508,7 +508,7 @@ for(i in 1:5){
     p1[j,2] = out2$pvalue
     p1[j,3] = out3$pvalue
     p1[j,4] = out4$pvalue
-    
+
     b1[j,1] = out$b.coef
     b1[j,2] = out2$b.coef
     b1[j,3] = out3$b.coef
@@ -516,16 +516,16 @@ for(i in 1:5){
 
   }
   print("...done!")
-  
+
   STM.median.p[i] = median(p1$simu1.p)
   LTM.median.p[i] = median(p1$simu2.p)
   TGM.median.p[i] = median(p1$simu3.p)
   GSS.median.p[i] = median(p1$simu4.p)
-  
-  
+
+
   storage1[[i]] = cbind.data.frame(p1, b1)
-  
-  
+
+
   if(plot.it==TRUE){
     png(paste0("/mnt/ceph/jarredk/GMACanalysis/Unstable_trios_plots/plot_", tissue, "_", trios[i], "_",mod.type.vec[i], ".png"))
     plot(p1, p3, pch=21, bg="black",
@@ -534,7 +534,7 @@ for(i in 1:5){
          main = paste0("GSS 1000 simulations of trio = ", trios[i], ";", tissue ))
     abline(a=0, b=1, lty="dotted", col="red")
     dev.off()
-  
+
     png(paste0("/mnt/ceph/jarredk/GMACanalysis/Unstable_trios_plots/histogram_", tissue, "_", trios[i], "_",mod.type.vec[i], ".png"))
     par(mfrow=c(2,2))
     hist(p1, xlab="Simulated Para. P-value",

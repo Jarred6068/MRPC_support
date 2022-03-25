10411609f5c0b272dd0f6fc936819a67b9ac2721
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2022-03-25 12:25:45 -0700

    Update Updated_PC_Selection.R

--- ADDIS_ReRun/Updated_PC_Selection/Updated_PC_Selection.R
+++ ADDIS_ReRun/Updated_PC_Selection/Updated_PC_Selection.R
@@ -1,7 +1,6 @@
 
 
 source("/mnt/ceph/jarredk/GMACanalysis/GMACpostproc.R")
-
 #get the top five tissues by sample size
 top5=c(1, 6, 33, 40, 48)
 path='/mnt/ceph/jarredk/Reg_Net/'
@@ -9,6 +8,8 @@ tissues.vec=tissue.names[top5, 2]
 library('psych', lib="/mnt/ceph/jarredk/Rpackages")
 
 for(i in 1:length(tissues.vec)){
+
+  print(paste0("Loading Data For...",tissues.vec[i]))
   PCs.matrix=loadRData(fileName=paste0("/mnt/lfs2/mdbadsha/peer_example/SNP_cis_trans_files/GTEx_version_8/",tissues.vec[i],"_AllPC/PCs.matrix.",tissues.vec[i],".RData"))
   data.all.unique.snps=loadRData(fileName=paste0("/mnt/lfs2/mdbadsha/peer_example/SNP_cis_trans_files/GTEx_version_8/",tissues.vec[i],
                                                  "_AllPC/data.snp.cis.trans.final.",tissues.vec[i],".V8.unique.snps.RData"))
@@ -17,8 +18,9 @@ for(i in 1:length(tissues.vec)){
   colnames(data.all.unique.snps) <- make.unique(colnames(data.all.unique.snps)) #duplicated colnames
 
   All.Pvalues <- list()
-  List.significant.asso1.AdiposeSubcutaneous <- list()
+  List.significant.asso1 <- list()
 
+  print(paste0("Getting PC List For...",tissues.vec[i]))
   # calculate associated PCs on all trios
   for (m in 1:(dim(PCs.matrix)[1]-1)) {
     corr.PCs <- corr.test(PCs.matrix[,m],data.all.unique.snps,use = 'pairwise.complete.obs', adjust="none")
@@ -26,14 +28,15 @@ for(i in 1:length(tissues.vec)){
     Pvalues <- corr.PCs$p
     Pvalues.nona <- Pvalues[!is.na(Pvalues)]
     All.Pvalues [[m]] <- Pvalues.nona
-    qobj <- qvalue(Pvalues.nona, fdr.level=0.10) 
-  
+    qobj <- qvalue(Pvalues.nona, fdr.level=0.10)
+
     # Significant associations
     Significant.asso <- qobj$significant
-    List.significant.asso1.AdiposeSubcutaneous[[m]] <- which(Significant.asso,useNames = TRUE)
+    List.significant.asso1[[m]] <- which(Significant.asso,useNames = TRUE)
   }
 
-  save(List.significant.asso1.AdiposeSubcutaneous, file=paste0("/mnt/ceph/jarredk/AddisReRunFiles/Updated_PC_Selection/List.significant.asso1.",tissues.vec[i],".RData"))
+  print(paste0("Saving List For...",tissues.vec[i]))
+  save(List.significant.asso1, file=paste0("/mnt/ceph/jarredk/AddisReRunFiles/Updated_PC_Selection/List.significant.asso1.",tissues.vec[i],".RData"))
 }
 
 
